790c0e3380b5de6f136e4d1ae21febe1f44a2256
Suman Nimbalkar <suman011@gmail.com>
2025-07-19 21:41:39 +0530

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ This line is critical for preventing blank page on Vercel
+  base: './', // ✅ required for Vercel to load assets/scripts correctly
 });
