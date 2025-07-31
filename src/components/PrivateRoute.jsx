e09909c0518a1faa20313fb5e993260e828f695f
Suman Nimbalkar <suman011@gmail.com>
2025-07-31 21:48:21 +0530

    removed files

--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/PrivateRoute.jsx
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function PrivateRoute({ allowedRoles, children }) {
-  const role = sessionStorage.getItem("userRole");
-  return allowedRoles.includes(role) ? children : <Navigate to="/" />;
-}
